9c0f21e81c50edcbe95e426ec226e37ff5e445f8
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-08-16 23:47:46 +0300

    addded filter

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,15 @@ $(function(){
   prevArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
   nextArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#arrow"></use></svg></button>'
   });
+
+  
+
+   $(".catalog-top__btn").on("click", function(){
+
+    $(".catalog-top__filter").toggleClass("catalog-top__filter--show");
+
+    $(".catalog-top__btn").toggleClass("catalog-top__btn--active");
+    
+   });
+
 });
